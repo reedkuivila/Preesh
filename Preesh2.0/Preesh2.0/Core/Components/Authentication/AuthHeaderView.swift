9f9eea51a26da9c7c6b285f2c7df210ba7ce16b8
reedkuivila <reedkuivila@uchicago.edu>
2023-10-24 10:59:32 -0500

    Bug fixes

--- Preesh2.0/Preesh2.0/Core/Components/Authentication/AuthHeaderView.swift
+++ Preesh2.0/Preesh2.0/Core/Components/Authentication/AuthHeaderView.swift
@@ -6,40 +6,6 @@
 //
 
 import SwiftUI
-//
-//struct AuthHeaderView: View {
-//    let title1: String
-//    let title2: String
-//
-//    var body: some View {
-//        VStack {
-//
-//            // header view
-//            VStack(alignment: .leading) {
-//                HStack { Spacer() }
-//
-//                Text(title1)
-//                    .font(.largeTitle)
-//                    .fontWeight(.semibold)
-//
-//                Text(title2)
-//                    .font(.title)
-//                    .fontWeight(.semibold)
-//            }
-//            .frame(height: 260)
-//            .padding(.leading)
-//            .background(Color("preeshBlue"))
-//            .foregroundColor(.white)
-//            .clipShape(RoundedShape(corners: [.bottomRight]))
-//        }
-//    }
-//}
-//
-//struct AuthHeaderView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        AuthHeaderView(title1: "hello", title2: "hello")
-//    }
-//}
 
 struct AuthHeaderView: View {
     let title1: String
